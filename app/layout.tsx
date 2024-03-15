a2a8dd2a6e6dd67489048b98a1e1d800cdc55217
Elmira <elmira.aras@protonmail.com>
2024-03-15 05:30:44 +0300

    update .gitignore

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import { martianMono } from "./ui/fonts";
+import './ui/global.css';
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={martianMono.className}>{children}</body>
     </html>
   );
 }
