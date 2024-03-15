c139f61a550323736d5103f2a2577300257e1150
Elmira <31357607+FearQuare@users.noreply.github.com>
2024-03-15 18:20:10 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
         if(entry === name){
           isValid = true;
           if(name === "Satsuki Shiga"){
-            setParagraphText(`${name} is not a developer but a WONDERFUL PERSON.`);
+            setParagraphText(`${name} is not a developer but a WONDERFUL WIFE.`);
           }else{
             setParagraphText(`${name} is one of the greatest developers.`);
           }
